048d33eb9d255216ca52123b55db4fa2a8d16304
Jonathan Castillo <jonathan@ygm.com>
2013-05-11 14:29:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashy::Application.config.secret_token = 'a537990c74e3e57ff66956da2304d7855cfedd09f70d4b0651664f8eb93736f34cf2b031a4127a9ffccdb3b5fdfa6e99532d66c0214f28cda6ca0ca7697c74ab'
